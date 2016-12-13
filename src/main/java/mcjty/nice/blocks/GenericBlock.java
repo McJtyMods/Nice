d635208a314225807767a9795767f92fa6263958
McJty <mcjty1@gmail.com>
2016-12-13 05:32:06 +0100

    Better item renderer

--- src/main/java/mcjty/nice/blocks/GenericBlock.java
+++ src/main/java/mcjty/nice/blocks/GenericBlock.java
@@ -2,11 +2,17 @@ package mcjty.nice.blocks;
 
 import mcjty.lib.compat.CompatBlock;
 import mcjty.nice.Nice;
+import mcjty.nice.client.BlockColor;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.BlockRendererDispatcher;
+import net.minecraft.client.renderer.ItemMeshDefinition;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraftforge.fml.relauncher.Side;
@@ -14,6 +20,7 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
+import java.util.Map;
 
 public class GenericBlock extends CompatBlock {
 
@@ -32,18 +39,19 @@ public class GenericBlock extends CompatBlock {
 
     @SideOnly(Side.CLIENT)
     public void initClient() {
-//        ModelLoader.setCustomMeshDefinition(Item.getItemFromBlock(this), new ItemMeshDefinition() {
-//            @Override
-//            public ModelResourceLocation getModelLocation(ItemStack stack) {
-//                int col = 0;
-//                if (stack.hasTagCompound()) {
-//                    col = stack.getTagCompound().getInteger("color");
-//                }
-//                BlockColor color = BlockColor.values()[col];
-//                return new ModelResourceLocation(getRegistryName(), "inventory");
-//            }
-//        });
-        ModelLoader.setCustomModelResourceLocation(Item.getItemFromBlock(this), 0, new ModelResourceLocation(getRegistryName(), "inventory"));
+        ModelLoader.setCustomMeshDefinition(Item.getItemFromBlock(this), new ItemMeshDefinition() {
+            @Override
+            public ModelResourceLocation getModelLocation(ItemStack stack) {
+                int col = 0;
+                if (stack.hasTagCompound()) {
+                    col = stack.getTagCompound().getInteger("color");
+                }
+                BlockColor color = BlockColor.values()[col];
+                BlockRendererDispatcher dispatcher = Minecraft.getMinecraft().getBlockRendererDispatcher();
+                Map<IBlockState, ModelResourceLocation> variants = dispatcher.getBlockModelShapes().getBlockStateMapper().getVariants(GenericBlock.this);
+                return variants.get(GenericBlock.this.getDefaultState().withProperty(GenericParticleBlock.COLOR, color));
+            }
+        });
     }
 
     private ItemBlock createItemBlock(Class<? extends ItemBlock> itemBlockClass) {
