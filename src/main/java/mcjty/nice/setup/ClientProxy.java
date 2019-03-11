8cc82dca63648bbeadd30288848213182cc2fc58
McJty <mcjty1@gmail.com>
2019-03-11 11:27:11 +0100

    New ClientRegistration class

--- src/main/java/mcjty/nice/setup/ClientProxy.java
+++ src/main/java/mcjty/nice/setup/ClientProxy.java
@@ -3,30 +3,19 @@ package mcjty.nice.setup;
 import mcjty.lib.setup.DefaultClientProxy;
 import mcjty.nice.Nice;
 import mcjty.nice.blocks.ModBlocks;
-import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.client.model.obj.OBJLoader;
-import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 public class ClientProxy extends DefaultClientProxy {
     @Override
     public void preInit(FMLPreInitializationEvent e) {
         super.preInit(e);
-        MinecraftForge.EVENT_BUS.register(this);
 
         OBJLoader.INSTANCE.addDomain(Nice.MODID);
 //        ModelLoaderRegistry.registerLoader(new BakedModelLoader());
 
     }
-
-    @SubscribeEvent
-    public void registerModels(ModelRegistryEvent event) {
-        // Typically initialization of models and such goes here:
-        ModBlocks.initModels();
-    }
-
 
     @Override
     public void init(FMLInitializationEvent e) {
